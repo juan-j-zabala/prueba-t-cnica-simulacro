89c3f3a79ce407224ac0531c122e68f22a50210a
juan jose zabala <juanzabala317@hotmail.com>
2023-11-12 17:40:13 -0500

    commit #3 rama1

--- src/libros-disponibles/Lbdisponibles.jsx
+++ src/libros-disponibles/Lbdisponibles.jsx
@@ -1,31 +1,32 @@
 const Lbdisponibles = ({ ...prop }) => {
   const { showlistbooks, setShowlistbooks } = prop;
+  const ANIMATION_TRIGGER = showlistbooks === true;
 
   return (
     <div
       className={` bg-white md:h-[100%] rounded-xl overflow-auto transition-all duration-300 ease-in-out delay-100  
     ${
-      showlistbooks === true ? " h-[10%] md:w-[70%] overflow-hidden  " : "w-[100%] md:w-[100%] "
+      showlistbooks === true ? " h-[11%] md:w-[70%] " : " h-[90%] w-[100%] md:w-[100%] "
     }  `}
     >
       <div className="flex flex-col gap-2 justify-center p-1">
-        <div className="bg-red-500  h-[10vh] w-full rounded-xl ">
+        <div className="bg-red-500 h-[10vh] w-full rounded-xl ">
           {/* <div className="bg-green-200 h-[8vh] "></div> */}
-        </div>
-        <div className=" p-1 h-[75%] rounded-xl grid gap-4 auto-rows-[22rem] grid-cols-[repeat(auto-fill,minmax(15rem,1fr))]">
-          <div className="bg-blue-500 rounded-lg flex justify-end ">
           <button onClick={() => setShowlistbooks(!showlistbooks)}>
               animation
             </button>
+        </div>
+        <div className={`p-1 h-[75%] rounded-xl grid gap-4 auto-rows-[22rem] grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] ${ANIMATION_TRIGGER ? "hidden" : "" } `} >
+          <div className="bg-blue-500 rounded-lg   ">
           </div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
-          <div className="bg-blue-500 rounded-lg flex justify-end "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
+          <div className="bg-blue-500 rounded-lg "></div>
 
         </div>
       </div>
--- src/lista-lecturas/LitLectura.jsx
+++ src/lista-lecturas/LitLectura.jsx
@@ -5,11 +5,11 @@ const LitLectura = ({ ...prop }) => {
 
   return (
     <div
-      className={`bg-white rounded-xl mt-1 md:mt-0 transition-all duration-300 ease-in-out delay-100 
+      className={`bg-white rounded-xl mt-1  md:mt-0 transition-all duration-300 ease-in-out delay-100 
     ${
       ANIMATION_TRIGGER
-        ? "md:h-[100%] md:w-[30%] h-[90%] "
-        : "absolute right-0 md:h-[0%] md:w-[0%]"
+        ? "md:h-[100%] md:w-[30%] h-[88%] "
+        : "h-[9%] md:absolute md:right-0 md:h-[0%] md:w-[0%]"
     } `}
     >
       <p className={`${ANIMATION_TRIGGER ? "" : "hidden"} `}>
